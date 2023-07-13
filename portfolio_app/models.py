58d7f6ca0e12eb79ee96f601a82701ca56a7f9d1
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-07-13 08:25:28 +0200

    Create Donation model

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -17,3 +17,17 @@ class Institution(models.Model):
     description = models.TextField()
     types = models.IntegerField(choices=TYPES, default=0)
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.TextField()
+    phone_number = models.TextField()
+    city = models.TextField()
+    zip_code = models.TextField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
