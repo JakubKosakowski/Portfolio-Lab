2be66a682dd845204b5fbf2d6754c06caff05206
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-07-13 08:12:20 +0200

    Create Institution model

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -4,3 +4,16 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.TextField()
+
+
+class Institution(models.Model):
+    TYPES = (
+        (0, "Fundacja"),
+        (1, "Organizacja pozarządowa"),
+        (2, "Zbiórka lokalna"),
+    )
+
+    name = models.TextField()
+    description = models.TextField()
+    types = models.IntegerField(choices=TYPES, default=0)
+    categories = models.ManyToManyField(Category)
