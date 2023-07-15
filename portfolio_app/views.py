86a698d58beaaec0655d8b85de74922f7c61ccd5
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-07-15 10:43:16 +0200

    Show all organizations connected with site

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -9,12 +9,17 @@ class LandingPageView(View):
     def get(self, request):
         received_sacks = 0
         supported_institutions = 0
+        all_organizations = {0: [], 1: [], 2: []}
         for donation in Donation.objects.all():
             received_sacks += donation.quantity
         for institution in Institution.objects.all():
             supported_institutions += 1 if len(institution.donation_set.all()) > 0 else 0
+            all_organizations[institution.types].append(institution)
         return render(request, 'index.html',
-                      {'received_sacks': received_sacks, 'supported_institutions': supported_institutions})
+                      {'received_sacks': received_sacks, 'supported_institutions': supported_institutions,
+                       'foundations': all_organizations[0],
+                       'non_gov_organizations': all_organizations[1],
+                       'local_collections': all_organizations[2]})
 
 
 class LoginView(View):
