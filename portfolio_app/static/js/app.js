99d80015933619e84a00a7a191332d7e7ae06663
Jakub Kosakowski <jkosakowski602@gmail.com>
2023-07-17 11:35:22 +0200

    Modify summary of donation in step 5

--- portfolio_app/static/js/app.js
+++ portfolio_app/static/js/app.js
@@ -254,8 +254,9 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
+let data = [];
 document.querySelector(".choose-categories > .btn").onclick = function(){
-  let data = [];
+  data = [];
   let markedCheckbox = document.getElementsByName('categories');
   let categories = document.getElementsByName('organization_categories');
   for (let checkbox of markedCheckbox){
@@ -263,6 +264,7 @@ document.querySelector(".choose-categories > .btn").onclick = function(){
       data.push(checkbox.nextElementSibling.nextElementSibling.nextElementSibling.value);
     }
   }
+  console.log(data)
   for(let i=0; i < categories.length; i++){
     let temp = false;
     for (let j = 0; j < data.length; j++){
@@ -277,5 +279,32 @@ document.querySelector(".choose-categories > .btn").onclick = function(){
     else{
       categories[i].parentElement.style.display = "None";
     }
+  }
+}
+
+document.querySelector('.submit-form > .next-step').onclick = function(){
+  let amountOfSacks = document.getElementById("bags").value;
+  let institution = document.querySelector('input[name="organization"]:checked').nextElementSibling.nextElementSibling.children[0].innerText;
+  const address = document.querySelector("#address");
+  const dateOfCollection = document.querySelector("#date-of-collection");
+  const sacks = document.querySelector("span.sacks");
+  const inst = document.querySelector("span.institution");
+  inst.innerText = `Dla organizacji "${institution}"`;
+  sacks.innerText = `Worki w ilości ${amountOfSacks}, zawierające `;
+  for (let i=0; i < data.length - 1; i++){
+    sacks.innerText += `${data[i]}, `;
+  }
+  sacks.innerText += `${data[data.length - 1]}`;
+  address.innerHTML = `<li>${document.querySelector('input[name="address"]').value}</li>
+                       <li>${document.querySelector('input[name="city"]').value}</li>
+                       <li>${document.querySelector('input[name="postcode"]').value}</li>
+                       <li>${document.querySelector('input[name="phone"]').value}</li>`
+  dateOfCollection.innerHTML = `<li>${document.querySelector('input[name="data"]').value}</li>
+                                <li>${document.querySelector('input[name="time"]').value}</li>`
+  if (document.querySelector('textarea[name="more_info"]').value === ""){
+    dateOfCollection.innerHTML += `<li>Brak uwag</li>`
+  }
+  else{
+    dateOfCollection.innerHTML += `<li>${document.querySelector('input[name="more_info"]').value}</li>`
   }
 }
